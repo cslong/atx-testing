164dacc3488611d5304c8cdb2ab54a87866d785d
Chris Long <longachr@amazon.com>
2025-10-31 10:44:23 -0700

    add .net 8 test sln

---
+++ net8testartifact/sourceCode/ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string Method1()
+        {
+            return "This is a test method for .NET 6";
+        }
+    }
+}
---
+++ net8testartifact/sourceCode/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+var projectPath =
+    "D:\\Users\\longachr\\source\\repos\\TestDotNet10ClassLibrary\\TestDotNet10ClassLibrary\\TestDotNet10ClassLibrary.csproj";
+
+var process = new Process
+{
+    StartInfo = new ProcessStartInfo
+    {
+        FileName = "dotnet",
+        Arguments = $"restore \"{projectPath}\"",
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        UseShellExecute = false,
+        CreateNoWindow = true
+    }
+};
+
+process.Start();
+
+string output = process.StandardOutput.ReadToEnd();
+string error = process.StandardError.ReadToEnd();
+
+process.WaitForExit();
+
+Console.WriteLine(output);
+if (!string.IsNullOrEmpty(error))
+{
+    Console.WriteLine($"Error: {error}");
+}
+
+Console.WriteLine(process.ExitCode); 
